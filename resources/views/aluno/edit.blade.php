2dfe8913a04c9c88ed06ebd68bffaa9e816cf454
Pedro Couto <pedrocouto152001@gmail.com>
2023-02-06 14:02:18 -0300

    correção de update do aluno

--- resources/views/aluno/edit.blade.php
+++ resources/views/aluno/edit.blade.php
@@ -12,23 +12,64 @@
                     <form action="{{route('aluno.update', $obj->id)}}" method="POST">
                         @csrf
                         <div class="form-group">
-                            @foreach($arrayObj as $item)
-                                <div class="py-2">
-                                    <label for="disabledTextInput">{{$item['label']}}</label>
-                                    <input name="{{$item['nameAttribute']}}" type="text" id="disabledTextInput" class="form-control rounded" value="{{$obj[$item['nameAttribute']]}}">
-                                </div>
-                            @endforeach
+
+                            <div class="py-2">
+                                <label for="disabledTextInput">Nome</label>
+                                <input name="nome" type="text" class="form-control rounded" value="{{$obj['nome']}}">
+                            </div>
+
+                            <div class="py-2">
+                                <label for="disabledTextInput">E-mail</label>
+                                <input name="email" type="email" class="form-control rounded" value="{{$obj['email']}}">
+                            </div>
+
+                            <div class="py-2">
+                                <label for="disabledTextInput">Telefone</label>
+                                <input name="telefone" type="text" class="form-control rounded" value="{{$obj['telefone']}}">
+                            </div>
+
+                            <div class="py-2">
+                                <label for="disabledTextInput">Data de nascimento</label>
+                                <input name="data_nascimento" type="date" class="form-control rounded" value="{{$obj['data_nascimento']}}">
+                            </div>
+
+                            <div class="py-2">
+                                <label for="disabledTextInput">Data de cadastro</label>
+                                <input disabled type="date" class="form-control rounded" value="{{$obj['data_cadastro']}}">
+                            </div>
+
+                            <div class="py-2">
+                                <label for="disabledTextInput">Data de pagamento</label>
+                                <input name="data_pagamento" type="date" class="form-control rounded" value="{{$obj['data_pagamento']}}">
+                            </div>
+
+                            <div class="py-2">
+                                <label for="disabledTextInput">Data de validade</label>
+                                <input name="data_vencimento" type="date" class="form-control rounded" value="{{$obj['data_vencimento']}}">
+                            </div>
+
+                            <hr class="my-3">
 
 
-                            @foreach($arrayRelations as $item)
-                                <div class="py-2">
-                                    <label for="disabledTextInput">{{$item['label']}}</label>
-                                    <input name="{{$item['nameAttribute']}}" type="text" id="disabledTextInput" class="form-control rounded" value="{{$obj['endereco'][$item['nameAttribute']]}}">
-                                </div>
-                            @endforeach
+                            <div class="py-2">
+                                <label for="disabledTextInput">Cidade</label>
+                                <input name="cidade" type="text" class="form-control rounded" value="{{$obj['enderecoAluno']->cidade}}">
+                            </div>
+                            <div class="py-2">
+                                <label for="disabledTextInput">Bairro</label>
+                                <input name="bairro" type="text" class="form-control rounded" value="{{$obj['enderecoAluno']->bairro}}">
+                            </div>
+                            <div class="py-2">
+                                <label for="disabledTextInput">Rua</label>
+                                <input name="rua" type="text" class="form-control rounded" value="{{$obj['enderecoAluno']->rua}}">
+                            </div>
+                            <div class="py-2">
+                                <label for="disabledTextInput">Número</label>
+                                <input name="numero" type="number" class="form-control rounded" value="{{$obj['enderecoAluno']->numero}}">
+                            </div>
+
                         </div>
-
-                        <button class="btn btn-primary" type="submit">Confirmar alterações</button>
+                        <button class="btn btn-primary my-3" type="submit">Confirmar alterações</button>
 
                     </form>
                 </div>
